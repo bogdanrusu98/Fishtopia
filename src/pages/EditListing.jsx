b479738f8223ee6a64b8c9d9e9451790293fae2d
Rusu Bogdan <bogdanrusu98@yahoo.ro>
2024-09-26 17:01:15 +0300

    spinner

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -19,7 +19,8 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import { Editor } from '@tinymce/tinymce-react';
-
+import axios from 'axios';
+import Map from '../components/Map';
 
 function EditListing() {
   const editorRef = useRef(null);
@@ -207,14 +208,21 @@ function EditListing() {
   };
 
   if (loading) {
-    return "Loading...";
+    return (
+      <div class="text-center">
+      <div role="status">
+          <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span class="sr-only">Loading...</span>
+      </div>
+  </div>
+    );
   }
 
   return (
     <div className="profile">
-      <header>
-        <p className="text-2xl font-bold">Edit Listing</p>
-      </header>
 
       <main>
         <form
@@ -226,99 +234,61 @@ function EditListing() {
           </h2>
 
           <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200"
-            >
-              Fish Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={onMutate}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-          </div>
+    <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Title</label>
+    <input
+      type="text"
+      id="title"
+      value={title}
+      onChange={onMutate}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      required
+    />
+  </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="title"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200"
-            >
-              title
-            </label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={onMutate}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-          </div>
+  <div className="mb-4">
+    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Fish Name</label>
+    <input
+      type="text"
+      id="name"
+      value={name}
+      onChange={onMutate}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      required
+    />
+  </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="risk"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200"
-            >
-              Risk Level
-            </label>
-            <input
-              type="text"
-              id="risk"
-              value={risk}
-              onChange={onMutate}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-          </div>
+  <div className="mb-4">
+    <label htmlFor="length" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Length (cm)</label>
+    <input
+      type="text"
+      id="length"
+      value={length}
+      onChange={onMutate}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      required
+    />
+  </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="length"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200"
-            >
-              Length (cm)
-            </label>
-            <input
-              type="text"
-              id="length"
-              value={length}
-              onChange={onMutate}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-          </div>
+  <div className="mb-4">
+    <label htmlFor="weight" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Weight (kg)</label>
+    <input
+      type="text"
+      id="weight"
+      value={weight}
+      onChange={onMutate}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      required
+    />
+  </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="weight"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200"
-            >
-              Weight (kg)
-            </label>
-            <input
-              type="text"
-              id="weight"
-              value={weight}
-              onChange={onMutate}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              required
-            />
-          </div>
-
-  
-          <div className="mb-4">
+  <div className="mb-4">
   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Description</label>
   <Editor
     apiKey="dphia909nnnrxny7iltrwnyxx7u62ht0wdo9hmviei90lkt7"
     onInit={(_evt, editor) => editorRef.current = editor}
-    initialValue="<p>This is the initial content of the editor.</p>"
+    initialValue=""
     init={{
-      height: 500,
+      height: 150,
       menubar: false,
       plugins: [
         'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
@@ -339,28 +309,22 @@ function EditListing() {
   />
         </div>
 
+   
 
-          <div className="mb-6">
-            <label
-              htmlFor="images"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200"
-            >
-              Upload Image
-            </label>
-            <input
-              type="file"
-              id="images"
-              onChange={onMutate}
-              max="6"
-              accept=".jpg, .png, .jpeg, .JPG, .PNG"
-              multiple
-              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-              required
-            />
-            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">
-              The first image will be the cover (max 6).
-            </p>
-          </div>
+  <div className="mb-6">
+    <label htmlFor="images" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Upload Image</label>
+    <input
+      type="file"
+      id="images"
+      onChange={onMutate}
+      max='6'
+      accept=".jpg, .png, .jpeg, .JPG, .PNG"
+      multiple
+      className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+      required
+    />
+    <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">The first image will be the cover (max 6).</p>
+  </div>
 
           <button
             type="submit"
